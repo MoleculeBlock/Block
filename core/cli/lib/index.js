3703195187a12c3a1a75e65ca36b0d6d81439f90
tarosun <sunxiaomingyes@gmail.com>
2021-02-19 21:20:51 +0800

    feat: 检查版本号功能、npmlog封装

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,7 +1,15 @@
 'use strict';
 
+const pkg = require('../package.json')
+const log = require('@atuo/cli-log')
+
 module.exports = core;
 
 function core() {
-    console.log('exec core')
+    checkPkgVersion()
+}
+
+// 检查版本号
+function checkPkgVersion() {
+    log.info('cli', `V${pkg.version}`)
 }
