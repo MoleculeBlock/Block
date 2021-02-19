f3a6c484c1c64c74af972821079e63b16661eda1
tarosun <sunxiaomingyes@gmail.com>
2021-02-19 21:45:39 +0800

    feat: 最低Node版本检查功能

--- core/cli/lib/index.js
+++ core/cli/lib/index.js
@@ -1,15 +1,34 @@
 'use strict';
 
-const pkg = require('../package.json')
+const semver = require('semver')
+const colors = require('colors/safe')
 const log = require('@atuo/cli-log')
+const pkg = require('../package.json')
+const { LOWEST_NODE_VERSION } = require('./const')
 
 module.exports = core;
 
 function core() {
-    checkPkgVersion()
+    try {
+        checkPkgVersion()
+        checkNodeVersion()
+    } catch (error) {
+        log.error(error.message)
+    }
+
 }
 
 // 检查版本号
 function checkPkgVersion() {
     log.info('cli', `V${pkg.version}`)
+}
+
+// 检查node版本号
+function checkNodeVersion() {
+    // 获取当前node版本号
+    const currentVersion = process.version
+        // 比对最低版本号
+    if (!semver.gte(currentVersion, LOWEST_NODE_VERSION)) {
+        throw new Error(colors.red(`脚手架需要安装 V${LOWEST_NODE_VERSION} 以上版本的 Node.js`))
+    }
 }
